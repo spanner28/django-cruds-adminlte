300f6e2bd01ba11565a0ebf86000d00ccbad99d0
Charl Joseph Mert <charl@comxnetworks.com>
2018-11-07 16:21:53 +0200

    initialized datetimepicker datepicker timepicker

--- cruds_adminlte/static/js/cruds.js
+++ cruds_adminlte/static/js/cruds.js
@@ -1,7 +1,20 @@
 $(function(){
-      $('select').select2();
-      $('input').iCheck({
-        checkboxClass: 'icheckbox_minimal-blue',
-        radioClass: 'iradio_minimal-blue',
-      });
+	$('select').select2();
+	$('input').iCheck({
+		checkboxClass: 'icheckbox_minimal-blue',
+		radioClass: 'iradio_minimal-blue',
+	});
+	$('.datetimeinput').datetimepicker({
+		inline: true,
+                sideBySide: true,
+		format: 'yyyy-mm-dd hh:ii'
+	});
+	$('.timeinput').datetimepicker({
+		format: 'hh:ii',
+		language: 'en',
+		pick12HourFormat: true
+	});
+	$('.dateinput').datepicker({
+		format: 'yyyy-mm-dd'
+	});
 });
